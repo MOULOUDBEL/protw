a37f9719bf1df86e66ad478cd297133bfb0a2baa
chev <MOULOUDBEL>
2022-03-08 22:39:06 +0100

    10 . creation de la vue listeAnimaux.blade.php

--- hispets/resources/views/listeAnimaux.blade.php
+++ hispets/resources/views/listeAnimaux.blade.php
@@ -2,14 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
     <div id='root' >
 
     </div>
+    <div id='footer' >
+
+
+    </div>
+
 </body>
-<script src="{{asset('js/app.js')}}"></script>
 </html>
